519b8d9d0053eec2c20d90c7d285aa02b571a5a1
Pramith Rajapaksa <you@example.com>
2021-09-12 11:18:22 +0530

    new age calculation function  is created

--- check.js
+++ check.js
@@ -4,4 +4,14 @@ const city  = "Amsterdam"
 
 console.log(`Hi I am  ${name} and  my age is ${age} and I am from ${city}`) ;
 
-
+//todo
+//we are going to create a age cal function
+const  calcAge = ( ) => {
+    if(age===12){
+        console.log(`cant `);
+    }
+    else
+    {
+        console.log(`can`);
+    }
+}
