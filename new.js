298506d1d26780fa0c8901a830c1177bcd7c24f4
pramithrajapaksa <pramithrajapaksha69@gmail.com>
2021-10-02 23:47:04 +0530

    new

--- new.js
+++ new.js
@@ -1,11 +1,17 @@
 console.log(`this is merging here`);
 console.log(`I am Workingd`);
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 console.log(`new`);
 =======
+=======
+>>>>>>> Stashed changes
 const data ="dcdv";
 if (data === "undefined"){
     console.log(data);
 }
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
