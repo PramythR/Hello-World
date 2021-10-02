0d9d18c339ca1769fb0dd7587884cc741b648bd4
pramithrajapaksa <pramithrajapaksha69@gmail.com>
2021-10-02 20:11:47 +0530

    newl adde

--- new.js
+++ new.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log(`I am the main `);
-=======
-console.log(`thi is merging form devolop branch`);
->>>>>>> devolop
-=======
-console.log(`this is merging `);
->>>>>>> devolop
+console.log(`new resolver`)
