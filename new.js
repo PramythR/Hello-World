8f19e695182cff55710f324bc855c97c06122f26
pramithrajapaksa <pramithrajapaksha69@gmail.com>
2021-10-02 23:46:41 +0530

    new

--- new.js
+++ new.js
@@ -1,4 +1,11 @@
 console.log(`this is merging here`);
 console.log(`I am Workingd`);
+<<<<<<< Updated upstream
 
 console.log(`new`);
+=======
+const data ="dcdv";
+if (data === "undefined"){
+    console.log(data);
+}
+>>>>>>> Stashed changes
